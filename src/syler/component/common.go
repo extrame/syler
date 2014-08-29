ca3e8b8ab539616f4dc34da2bbb10bbf070a650f
jiang xu <zgdzdxxj@163.com>
2014-08-29 16:19:48 +0800

    secret改为计算md5后的密码

--- src/syler/component/common.go
+++ src/syler/component/common.go
@@ -16,9 +16,12 @@ type AuthInfo struct {
 var AuthingUser map[string]AuthInfo
 
 func CalcSecret(authinfo AuthInfo) string {
+	pwdhelper := md5.New()
+	pwds := pwdhelper.Sum(authinfo.Pwd)
+
 	md5helper := md5.New()
 	md5helper.Write(authinfo.Name)
-	md5helper.Write(authinfo.Pwd)
+	md5helper.Write(pwds)
 	md5helper.Write([]byte(*config.ProxyId))
 	md5helper.Write(authinfo.PublicKey)
 	return hex.EncodeToString(md5helper.Sum(nil))
